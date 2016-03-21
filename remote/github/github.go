6ef4072f5525fcab2a30ffc360acad6e62e65462
Qian Hong <qhong@codeweavers.com>
2016-03-22 01:04:35 +0800

    Rename continuous-integration/drone to continuous-integration/tea to avoid confusing users.

--- remote/github/github.go
+++ remote/github/github.go
@@ -248,7 +248,7 @@ func repoStatus(client *github.Client, r *model.Repo, b *model.Build, link strin
 	status := getStatus(b.Status)
 	desc := getDesc(b.Status)
 	data := github.RepoStatus{
-		Context:     github.String("continuous-integration/drone"),
+		Context:     github.String("continuous-integration/tea"),
 		State:       github.String(status),
 		Description: github.String(desc),
 		TargetURL:   github.String(link),
